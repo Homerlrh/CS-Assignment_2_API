4b4db1f438546365d0671d868b3f766d8ecdd702
homer li <homer.lee.0508@gmail.com>
2021-03-15 16:54:33 -0700

    first commit

---
+++ Assignment2/Repositories/UserRepo.cs
@@ -0,0 +1,30 @@
+﻿using Assignment2.Data;
+using Assignment2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Repositories
+{
+    public class UserRepo
+    {
+        ApplicationDbContext _context;
+
+        public UserRepo(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // Get all users in the database.
+        public IEnumerable<UserVM> All()
+        {
+            var users = _context.Users.Select(u => new UserVM()
+            {
+                Email = u.Email
+            });
+            return users;
+        }
+
+    }
+}
